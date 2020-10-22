9034eee4e372756edd34843f494cfe3bd4c1bc29
Michael Tsang <miklcct@gmail.com>
2020-10-22 10:53:51 +0800

    es6 syntax implementation

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function SelfReproducingClass(spy) {
-    if (spy !== undefined) {
-        spy();
+class SelfReproducingClass {
+    constructor(spy) {
+        if (spy !== undefined) {
+            spy();
+        }
+
+        const This = this.constructor;
+
+        // make the constructor of Derived calling the constructor of This
+        class Derived extends This {
+            constructor(...args) {
+                return Reflect.construct(This, args, new.target);
+            }
+        }
+
+        // make "new Derived" a subclass of "Derived"
+        Object.setPrototypeOf(Derived.prototype, Derived);
+
+        // make Derived an instance of This
+        Object.setPrototypeOf(Derived, This.prototype);
+
+        return Derived;
     }
 
-    const This = this.constructor;
-
-    // make the constructor of Derived calling the constructor of This
-    const Derived = function (...args) {
-        return This.apply(this, args);
-    };
-
-    // make Derived an instance of This
-    Object.setPrototypeOf(Derived, This.prototype);
-
-    // make "new Derived" a subclass of "Derived"
-    Object.setPrototypeOf(Derived.prototype, Derived);
-
-    return Derived;
+    static member = 0;
 }
+
 // make "new SelfReproducingClass" a subclass of SelfReproducingClass
 Object.setPrototypeOf(SelfReproducingClass.prototype, SelfReproducingClass);
-
-// member variable
-SelfReproducingClass.member = 0;
 
 module.exports = SelfReproducingClass;
